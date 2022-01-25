31956eb1382ccc70809715c7c4d4e7e48cc8912a
Brandon Learned <blearned92@gmail.com>
2022-01-25 14:52:48 -0700

    Condensed .hash() and .compress() into one function

--- script.py
+++ script.py
@@ -7,13 +7,10 @@ class HashMap():
       self.array = [LinkedList() for item in range(array_size)]
 
     def hash(self, key):
-      return sum(key.encode()) 
-
-    def compress(self, hash_code):
-      return hash_code % self.array_size
+      return sum(key.encode()) % self.array_size 
 
     def assign(self, key, value):
-      array_index = self.compress(self.hash(key))
+      array_index = self.hash(key)
       payload = Node([key, value])
       list_at_array = self.array[array_index]
       for item in list_at_array:
@@ -23,7 +20,7 @@ class HashMap():
       list_at_array.insert(payload)
 
     def retrieve(self, key):
-      array_index = self.compress(self.hash(key))
+      array_index = self.hash(key)
       list_at_index = self.array[array_index]
       for item in list_at_index:
         if key == item[0]:
